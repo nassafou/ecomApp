03fd4d0d4045214a3d94c8e06b54ecea7bbb3e80
nassafou <ntyoussouf@gmail.com>
2016-02-19 00:49:03 +0100

    commandes

---
+++ src/MyApp/EcommerceBundle/Entity/Commandes.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace MyApp\EcommerceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Commandes
+ *
+ * @ORM\Table(name="commandes")
+ * @ORM\Entity(repositoryClass="MyApp\EcommerceBundle\Repository\CommandesRepository")
+ */
+class Commandes
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="valider", type="boolean")
+     */
+    private $valider;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="reference", type="integer")
+     */
+    private $reference;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="produits", type="string", length=255)
+     */
+    private $produits;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set valider
+     *
+     * @param boolean $valider
+     * @return Commandes
+     */
+    public function setValider($valider)
+    {
+        $this->valider = $valider;
+
+        return $this;
+    }
+
+    /**
+     * Get valider
+     *
+     * @return boolean 
+     */
+    public function getValider()
+    {
+        return $this->valider;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Commandes
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set reference
+     *
+     * @param integer $reference
+     * @return Commandes
+     */
+    public function setReference($reference)
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    /**
+     * Get reference
+     *
+     * @return integer 
+     */
+    public function getReference()
+    {
+        return $this->reference;
+    }
+
+    /**
+     * Set produits
+     *
+     * @param string $produits
+     * @return Commandes
+     */
+    public function setProduits($produits)
+    {
+        $this->produits = $produits;
+
+        return $this;
+    }
+
+    /**
+     * Get produits
+     *
+     * @return string 
+     */
+    public function getProduits()
+    {
+        return $this->produits;
+    }
+}
